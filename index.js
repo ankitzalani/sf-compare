02ca81bc525fa3e85647860110980534e21f5019
Ankit Zalani <azalani@salesforce.com>
2020-07-14 18:51:04 +0530

    Initial changes

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var app = express();
+var cors = require('cors');
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+var bodyParser = require('body-parser');
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+//require('./server/index')(app);
+
+app.use(express.static(__dirname + '/client'));
+app.listen(port, ip, function() {
+    console.log("Listening on " + ip + ": " + port);
+});
