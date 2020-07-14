a818c3f4a5aae0760aba1e5136b48aef237925e2
Ankit Zalani <azalani@salesforce.com>
2020-07-14 19:42:06 +0530

    Changes for auth

--- server.js
+++ server.js
@@ -18,10 +18,32 @@ app.use(bodyParser.urlencoded({
 app.use(express.static(__dirname + '/client'));
 app.use(express.static(__dirname + '/lib'));
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.listen(port, ip, function() {
     console.log("Listening on " + ip + ": " + port);
 });
 
+
+
 app.get('/auth', function(req, res) {
     res.redirect('/');
 });
+
