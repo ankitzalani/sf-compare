e95942dd680fa3adf59cc5e039d23a3251ea4b08
Ankit Zalani <azalani@salesforce.com>
2020-07-14 19:47:32 +0530

    Changes for auth

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 var bodyParser = require('body-parser');
 
-//app.use(cors());
+app.use(cors());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
@@ -18,30 +19,9 @@ app.use(bodyParser.urlencoded({
 app.use(express.static(__dirname + '/client'));
 app.use(express.static(__dirname + '/lib'));
 
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-
 app.listen(port, ip, function() {
     console.log("Listening on " + ip + ": " + port);
 });
-
-
 
 app.get('/auth', function(req, res) {
     res.redirect('/');
